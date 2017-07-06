9a9e74c1a1389edc174c5cc53f0c0bdc6243f151
Xan Mead <x.m.mead@gmail.com>
2017-07-05 20:22:09 -0400

    text longer than 140 characters is now automatic turned into a thread

--- bot.py
+++ bot.py
@@ -14,7 +14,8 @@ logfile_name = bot_username + ".log"
 
 def create_tweet():
     """Create the text of the tweet you want to send."""
-    text = ".@godelescherbabe is gimblet-eyed gilly gaupus."
+    text = "The merit of Captain Grose's Dictionary of the Vulgar Tongue has been long and universally acknowledged. But its circulation was confined almost exclusively to the lower orders of society: he was not aware, at the time of its compilation, that our young men of fashion would at no very distant period be as distinguished for the vulgarity of their jargon as the inhabitants of Newgate; and he therefore conceived it superfluous to incorporate with his work the few examples of fashionable slang that might occur to his observation."
+    text = "In honor of the venerable Francis Grose."
     return text
 
 
@@ -25,14 +26,25 @@ def tweet(text):
     auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
     api = tweepy.API(auth)
 
+    # Break the tweet up as necessary into 140-character chunks
+    tweetset = []
+    while len(text) > 140:
+        tweetset.append(text[:140])
+        text = text[140:]
+    tweetset.append(text)
 
     # Send the tweet and log success or failure
-    try:
-        api.update_status(text)
-    except tweepy.error.TweepError as e:
-        log(e.message)
-    else:
-        log("Tweeted: " + text)
+    last_tweet = None
+    for status in tweetset:
+        try:
+            if last_tweet:
+                last_tweet = api.update_status(status, last_tweet.id)
+            else:
+                last_tweet = api.update_status(status)
+        except tweepy.error.TweepError as e:
+            log(e.message)
+        else:
+            log("Tweeted: " + status)
 
 
 def log(message):
